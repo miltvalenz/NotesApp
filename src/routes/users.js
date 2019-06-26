4e6444e162e0052a0c3f5448f83a1bf16fd1983a
milton alejandro <miltvalenz@hotmail.com>
2019-06-26 18:19:09 -0400

    Views

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 
 router.get('/signin', (req, res) => {
-    res.send('Ingresando a la app');
+    res.render('users/signin');
 });
 
 router.get('/signup', (req, res) => {
-    res.send('Formulario de autenticacion');
+    res.render('users/signup');
 });
 
 module.exports = router;
