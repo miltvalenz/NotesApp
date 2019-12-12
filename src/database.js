f9e18eb5d946d6774d103c0b8641d63e8c4c2a53
MiltonValenzuela <miltvalenz@hotmail.com>
2019-12-11 20:58:17 -0400

    Adding new url to mongodb atlas

--- src/database.js
+++ src/database.js
@@ -5,8 +5,9 @@ mongoose
 	.connect(url, {
 		useCreateIndex: true,
 		useNewUrlParser: true,
-        useFindAndModify: false,
-        useUnifiedTopology: true
+		useFindAndModify: false,
+        useUnifiedTopology: true,
+        dbName: 'notesApp'
 	})
 	.then(db => console.log('DB is connected'))
 	.catch(err => console.log(err));
